b7bd1360cacc4ed6361da8585931d07ef42ae609
Copift <copift@yandex.ru>
2024-10-02 18:07:04 +0800

    add led

--- detect_light/src/main.cpp
+++ detect_light/src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
 
-#define leftPin A1
-
+#define leftPin A5
 #define rightPin A0
+#define leftLed 6
+#define serviceLed 7
+#define rightLed 8
+
 
 void setup() {
+   pinMode(leftLed, OUTPUT);
+  pinMode(rightLed, OUTPUT);
+  pinMode(serviceLed, OUTPUT);
   Serial.begin(9600);
-  Serial.print("HELOO");
+  Serial.println("calibrate..");
 
+  // Устанавливаем пины в режим выхода
+ 
 }
+
+
 
 void loop() {
   int left = analogRead(leftPin);
