cf633a3f067f541b81cb8f225d033ea62501a4b3
Copift <copift@yandex.ru>
2024-10-02 19:11:04 +0800

    add calibrate on start

--- detect_light/src/main.cpp
+++ detect_light/src/main.cpp
@@ -5,19 +5,72 @@
 #define leftLed 6
 #define serviceLed 7
 #define rightLed 8
+// минимум максимум для датчиков
 
-
+// 0 - maxLeft 1 - minLeft 2 - maxRight 3 - minRight массив для хранения калибровок датчиков
+int values[] = {0, 0, 0,0}; 
+// время на калибровку
+int delayForCalibrate=5000;
+// время на смену позиции с светлого на черное
+int delayForSwithPos=1200;
 void setup() {
-   pinMode(leftLed, OUTPUT);
+  pinMode(leftLed, OUTPUT);
   pinMode(rightLed, OUTPUT);
   pinMode(serviceLed, OUTPUT);
+  digitalWrite(serviceLed,HIGH);
+  delay(delayForSwithPos);
   Serial.begin(9600);
-  Serial.println("calibrate..");
-
-  // Устанавливаем пины в режим выхода
  
-}
 
+  //переменные для цикла переключают  датчики  и светодиоды в цикле
+  int ledNow=leftLed;
+  int pinNow=leftPin;
+
+  // цикл калибровки
+  for (size_t i = 0; i < 4; i++)
+  {
+    //ждем смену состояния 
+    digitalWrite(serviceLed,LOW);
+    delay(delayForSwithPos);
+    digitalWrite(serviceLed,HIGH);
+    //переключаем пины
+    if (i==2){
+      ledNow=rightLed;
+      pinNow=rightPin;
+    }
+  // переключаем состояние с светолого на темное
+  if (i==1 or i ==3){
+    digitalWrite(ledNow,LOW);
+  }else {
+    digitalWrite(ledNow,HIGH);
+  }
+  //для подсчета среднего значения
+  long count=0;
+  long sum=0;
+  long startCalibrate=millis();
+  //цикл на калибровку одного состояния
+  while (startCalibrate+delayForCalibrate>=millis()){
+  
+   sum+= analogRead(pinNow);
+    count+=1;
+  }
+  //считаем среднее
+  values[i]=sum/count;
+  //Serial.println(values[i]);
+  digitalWrite(ledNow,LOW);
+
+}
+//выводим
+Serial.print("values");
+for (size_t i = 0; i < 4; i++)
+{
+  Serial.print(values[i]);
+  Serial.print("  ");
+  Serial.println();
+
+}
+  digitalWrite(serviceLed,LOW);
+}
 
 
 void loop() {
