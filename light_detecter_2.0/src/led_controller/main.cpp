f69446d22496830125f915563418e1da9ceb583b
Copift <copift@yandex.ru>
2024-10-07 22:15:11 +0800

    change input from str to byte

--- light_detecter_2.0/src/led_controller/main.cpp
+++ light_detecter_2.0/src/led_controller/main.cpp
@@ -1,19 +1,18 @@
 #include <Arduino.h>
-#define led 8
+#define led 13
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
   if(Serial.available()>0) {
-     String teststr = Serial.readString();
-    teststr.trim();
-    if (teststr=="u"){
+     int teststr = Serial.read();
+    if (teststr==117){//u
         digitalWrite(led,HIGH);
     }
-    if (teststr=="d"){
+    if (teststr==100){//d
         digitalWrite(led,LOW);
     }
-
+    Serial.println(teststr);
   }     
 }
