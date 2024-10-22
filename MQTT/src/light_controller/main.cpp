f9470e4d0574aca7875fadd8b64680737e7410fc
Copift <copift@yandex.ru>
2024-10-22 09:33:40 +0800

    add esp8266

--- MQTT/src/light_controller/main.cpp
+++ MQTT/src/light_controller/main.cpp
@@ -6,21 +6,16 @@ void setup() {
 }
 
 void loop() {
-
   if(Serial.available()>0){
     int teststr = Serial.read();
   
     if (teststr==112){
-    for (size_t i = 0; i < 100; i++)
-    {
+     
+    int left = analogRead(leftPin);
     
-        int left = analogRead(leftPin);
-  
 
-        Serial.write(left);
-       
+    Serial.println(left);
     }
-     }
     else{
         Serial.println("unknown command");
     }
--- light_detecter_2.0/src/python/test.py
+++ light_detecter_2.0/src/python/test.py
@@ -6,33 +6,17 @@ responses = {'d': 7,  # "led_off"
 port_led = "/dev/ttyUSB0" # "COM4" or similar for windows
 port_photo =  "/dev/ttyUSB0" # "COM4" or similar for windows
 #connection_led = serial.Serial(port_led, timeout=1) # baudrate=9600
-connection_photo = serial.Serial(port_photo, timeout=1,baudrate=1000000) # baudrate=9600
-count=0
-buf = 0
-c= 'p'.encode()
-time.sleep(6)
-start_time = time.time()
-def send_command(cmd: str, response_len: int, connection: serial.Serial) -> str:
-    global count
-    global start_time
-    global buf
-    global c
+connection_photo = serial.Serial(port_photo, timeout=1,baudrate=115200) # baudrate=9600
 
-    connection.write(c)
+def send_command(cmd: str, response_len: int, connection: serial.Serial) -> str:
+    connection.write(cmd.encode())
     if response_len > 0:
         # connection.in_waiting <-> available()
         resp = connection.read()
-    if count%1000==0:
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"The task took {elapsed_time:.2f} seconds to complete {count} values, last 1000 values in {round(end_time-buf,3)}")
-        buf = end_time
-    count+=1
     return resp
 
 while True:
     photo_val_resp: str = send_command('p', responses['p'], connection_photo)
-
     if photo_val_resp:
         photo_val =photo_val_resp
+        print(photo_val)
